ac7d822a6f4d771cd4018956d6e2332e8718fb92
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2017-09-15 09:32:18 +0200

    rename some variables

--- templates/update/lib/deployer/bootstrap-properties.php
+++ templates/update/lib/deployer/bootstrap-properties.php
@@ -23,34 +23,34 @@ $files = [
   'bin',
 ];
 
-$workflowProperties = [];
+$heavydProperties = [];
 
 foreach($files as $file) {
   $fullPath = $projectRootDir . '/properties/' . $file . '.yml';
   if (!file_exists($fullPath)) {
     throw new \Exception(sprintf('Required file %s not found, have you ensured that the properties were completed before starting the deploy. Has phing properties:write-full been run?', $fullPath));
   }
-  $workflowProperties[$file] =  Yaml::parse(file_get_contents($fullPath));
+  $heavydProperties[$file] =  Yaml::parse(file_get_contents($fullPath));
 }
 
 /*
  * Add various items to the active config.
  */
 // This needs to be made relative ...
-$relativeWebDir = str_replace($workflowProperties['project']['basePath'] . '/', '', $workflowProperties['dir']['web']['root']);
+$relativeWebDir = str_replace($heavydProperties['project']['basePath'] . '/', '', $heavydProperties['dir']['web']['root']);
 set('dir_web_relative', $relativeWebDir);
 
-$relativePhingDir = str_replace($workflowProperties['project']['basePath'] . '/', '', $workflowProperties['bin']['phing']);
+$relativePhingDir = str_replace($heavydProperties['project']['basePath'] . '/', '', $heavydProperties['bin']['phing']);
 set('bin_phing', $relativePhingDir);
 
 // used by some scripts to generate a better absolute url
-set('local_workspace', $workflowProperties['project']['basePath']);
+set('local_workspace', $heavydProperties['project']['basePath']);
 
 /*
  * Generate server instances out of the data contained in the server.yml file.
  * This is a more custom implementation to make the system more uniform.
  */
-foreach ($workflowProperties['server'] as $serverKey => $serverData) {
+foreach ($heavydProperties['server'] as $serverKey => $serverData) {
   server($serverKey, $serverData['host'])
     ->user($serverData['user'])
     ->set('deploy_path', $serverData['root'])
