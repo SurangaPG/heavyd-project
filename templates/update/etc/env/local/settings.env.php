81f3cbfb8298a99ec55db4209c23724b6eb36411
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2017-12-14 20:04:54 +0100

    make the local env more self sufficient

--- templates/update/etc/env/local/settings.env.php
+++ templates/update/etc/env/local/settings.env.php
@@ -1,37 +1,44 @@
 <?php
 
-/**
- * @file includes the needed settings form for a default setup.
- *
- * The default setup assumes 2 things:
- *
- * A .env file in the root of the project (e.g one step above the webroot) which
- * contains all the data for serverbased connections.
- * Such as the DB etc. This file should contain a base_64 encoded base string.
- *
- * A .host file in the root of the project (e.g one step above the webroot) which
- * contains all the data for the host based protection etc. This should be a
- * json formatted file.
- * Note that the heavyd deploy will generate this for you at build time.
- *
- * The main distinctions between both files are:
- * - .env contains sensitive serverspecific data (passwords etc) and should never move of server
- * - .host contains "semi" serverspecific data such as Domains etc and should be able to migrate but is still
- *   defined on the server level.
+// Generate a dummy local hash
+$dummy_hash = [];
+exec('whoami', $dummy_hash);
+$settings['hash_salt'] = base64_encode($dummy_hash[0] . __DIR__);
+
+// Read the machine name from the current dir
+$baseSiteName = basename(__DIR__);
+
+/*
+ * Read all the other data from the db properties.
  */
+$dbPropertiesFile = dirname(dirname(dirname(__DIR__))) . '/properties/db.yml';
 
-// Get the DB info etc from the .env file
-$envFile = dirname(dirname(dirname(__DIR__))) . '/.env.json';
+$dbPropertieSets = ['default' => ['default' => []]];
+if (file_exists($dbPropertiesFile)) {
+  $dbPropertieSets = \Symfony\Component\Yaml\Yaml::parse(
+    file_get_contents($dbPropertiesFile)
+  );
+}
 
-$envConf = file_get_contents($envFile);
-$envConf = json_decode($envConf, TRUE);
-$envConf = $envConf[basename(__DIR__)];
+// Add default database settings if not given
+foreach ($dbPropertieSets[$baseSiteName] as $key1 => $databaseList) {
+  foreach ($databaseList as $key2 => $database) {
+    // check if port is given
+    if (!isset($dbPropertieSets[$baseSiteName][$key1][$key2]['port'])) {
+      $dbPropertieSets[$baseSiteName][$key1][$key2]['port'] = 3306;
+    }
 
-$databases = $envConf['databases'];
+    // check if driver is given
+    if (!isset($dbPropertieSets[$baseSiteName][$key1][$key2]['driver'])) {
+      $dbPropertieSets[$baseSiteName][$key1][$key2]['driver'] = 'mysql';
+    }
 
-$settings['hash_salt'] = $envConf['hash_salt'];
+    // check if namespace is given
+    if (!isset($dbPropertieSets[$baseSiteName][$key1][$key2]['namespace'])) {
+      $dbPropertieSets[$baseSiteName][$key1][$key2]['namespace'] = "Drupal\\Core\\Database\\Driver\\mysql";
+    }
+  }
+}
 
-// Get the extra information from the host file.
-// @TODO Finish this.
-$hostFile = dirname(dirname(dirname(__DIR__))) . '/.host';
-$hostConf = json_decode(file_get_contents($hostFile), TRUE);
+// Set databases
+$databases = $dbPropertieSets[$baseSiteName];
