59f993d6cc50f4d028389c35643bc212272c3418
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2018-03-16 19:19:28 +0100

    add extra helper properties.

--- dev/phing-tasks/GenerateDocumentationTask.php
+++ dev/phing-tasks/GenerateDocumentationTask.php
@@ -10,6 +10,29 @@ require_once "phing/Task.php";
  */
 class GenerateDocumentationTask extends Task {
 
+  /**
+   * @var string
+   *   Main directory to place the output.
+   */
+  private $outputDir;
+
+  /**
+   * @var string
+   *   The main task file for the project.
+   */
+  private $mainTaskFile;
+
+  /**
+   * @var string
+   *   The dir with all the phing subtasks.
+   */
+  private $subTaskDir;
+
+  /**
+   * @var string
+   *   The dir with all the partial html files.
+   */
+  private $htmlPartialsDir;
 
   /**
    * The init method: Do init steps.
@@ -24,6 +47,62 @@ class GenerateDocumentationTask extends Task {
   public function main() {
 
 
+  }
+
+  /**
+   * @return string
+   */
+  public function getHtmlPartialsDir() {
+    return $this->htmlPartialsDir;
+  }
+
+  /**
+   * @return string
+   */
+  public function getMainTaskFile() {
+    return $this->mainTaskFile;
+  }
+
+  /**
+   * @return string
+   */
+  public function getOutputDir() {
+    return $this->outputDir;
+  }
+
+  /**
+   * @return string
+   */
+  public function getSubTaskDir() {
+    return $this->subTaskDir;
+  }
+
+  /**
+   * @param $htmlPartialsDir
+   */
+  public function setHtmlPartialsDir($htmlPartialsDir) {
+    $this->htmlPartialsDir = $htmlPartialsDir;
+  }
+
+  /**
+   * @param $mainTaskFile
+   */
+  public function setMainTaskFile($mainTaskFile) {
+    $this->mainTaskFile = $mainTaskFile;
+  }
+
+  /**
+   * @param $outputDir
+   */
+  public function setOutputDir($outputDir) {
+    $this->outputDir = $outputDir;
+  }
+
+  /**
+   * @param $subTaskDir
+   */
+  public function setSubTaskDir($subTaskDir) {
+    $this->subTaskDir = $subTaskDir;
   }
 }
 
