db3cdfd3aa6349c4be5916b390d064c06b914576
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2018-03-16 19:13:23 +0100

    add basic documentation helper

---
+++ dev/phing-tasks/GenerateDocumentationTask.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "phing/Task.php";
+
+/**
+ * Class GenerateDocumentationTask
+ *
+ * Generate all the documentation for the various phing targets and
+ * write them to html files.
+ */
+class GenerateDocumentationTask extends Task {
+
+
+  /**
+   * The init method: Do init steps.
+   */
+  public function init() {
+    // nothing to do here
+  }
+
+  /**
+   * The main entry point method.
+   */
+  public function main() {
+
+
+  }
+}
+
+?>
